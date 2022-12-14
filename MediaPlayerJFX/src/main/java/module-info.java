615577e37cd826c6c9a107863290ec5d7c49a6bd
Mateusz Sidor <110628257+Matiar15@users.noreply.github.com>
2022-12-14 20:16:08 +0100

    Updated modules

--- MediaPlayerJFX/src/main/java/module-info.java
+++ MediaPlayerJFX/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.example.mediaplayerjfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
+    requires mp3agic;
+    requires java.desktop;
+    requires javafx.swing;
 
     opens com.example.mediaplayerjfx to javafx.fxml;
     exports com.example.mediaplayerjfx;
